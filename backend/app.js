d4d34983f6378a1928ebdd9d0dcc0e874c019426
Joshua <joshua.hulford@gmail.com>
2023-04-18 12:01:39 -0400

    Add test route to app.js

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,14 @@ if (!isProduction) {
     })
   );
 
+// for testing
+  app.get('/spots', async (req, res) => {
+    const { Spot } = require('../backend/db/models');
+    const spots = await Spot.findAll();
+    console.log(spots)
+    res.json(spots)
+  });
+
   app.use(routes); // Connect all the routes
 
 
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -28,6 +28,7 @@ const validateSignup = [
     handleValidationErrors
   ];
 
+
 // Sign up
 router.post(
     '', validateSignup,
