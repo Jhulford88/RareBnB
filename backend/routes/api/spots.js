5f0a35b23fcd19a7aa810e45c112097c6e25d7bd
Joshua <joshua.hulford@gmail.com>
2023-04-18 19:29:30 -0400

    add endpoint to get spots owned by current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,8 +1,56 @@
 const express = require('express');
 const { Spot, Review, SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js');
 const router = express.Router();
 
+//get all spots owned by the current user  req.user.id
+router.get('/current', requireAuth, async (req, res) => {
 
+    const spots = await Spot.findAll({
+        where: {
+          ownerId: req.user.id
+        },
+        include: [
+        { model: Review },
+        { model: SpotImage }
+    ]
+    });
+
+    let spotList = []
+
+    spots.forEach(spot => {
+        spotList.push(spot.toJSON())
+    })
+
+    spotList.forEach(spot => {
+        let sum = 0;
+        spot.Reviews.forEach(review => {
+            sum += review.stars
+        })
+        spot.avgRating = sum / spot.Reviews.length
+
+        delete spot.Reviews
+    })
+
+
+    spotList.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                spot.previewImage = image.url
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = 'No image found'
+        }
+        delete spot.SpotImages
+    })
+
+
+    res.json(spotList)
+
+})
+
+//get all spots and include preview images and average ratings
 router.get('', async (req, res) => {
 
     const spots = await Spot.findAll({
