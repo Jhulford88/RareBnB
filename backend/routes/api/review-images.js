a23eb43cf8cdc6867be5c88093b4e6a0bfab98fc
Joshua <joshua.hulford@gmail.com>
2023-04-21 09:23:24 -0400

    Add endpoint for delete review image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -6,7 +6,32 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 
+//Delete spot image
+router.delete('/:imageId', requireAuth, async (req, res) => {
 
+    //find review image by imageId
+    const reviewImage = await ReviewImage.findByPk(req.params.imageId, {
+        include: {
+            model: Review
+        }
+    });
+
+    //check that review image exists
+    if (!reviewImage) {
+        return res.status(404).json({ "message": "Review Image couldn't be found"});
+    };
+
+    //check authorization of user
+    const {user} = req;
+    const normalizedUser = user.toJSON();
+    if (reviewImage.Review.userId !== normalizedUser.id) {
+        return res.status(403).json({"message": "Forbidden"});
+    };
+
+    //delete review image
+    await reviewImage.destroy();
+    res.json({ "message": "Successfully deleted"})
+});
 
 
 
