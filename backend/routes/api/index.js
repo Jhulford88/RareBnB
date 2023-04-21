d46ef0965e8b53a2e717b0bf14be560d514787f2
Joshua <joshua.hulford@gmail.com>
2023-04-21 08:28:54 -0400

    Add router for spot images and connect to app

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,6 +7,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const spotImagesRouter = require('./spot-images.js')
 
 router.use(restoreUser);
 
@@ -19,6 +20,8 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
+
+router.use('/spot-images', spotImagesRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
