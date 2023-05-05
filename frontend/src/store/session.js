bcc40491ed3f7edd020c0a037d84390b83d09a7a
Joshua <joshua.hulford@gmail.com>
2023-05-05 13:05:24 -0400

    complete begining setup phase

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,8 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+
+//Action Creators
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -16,6 +18,7 @@ const removeUser = () => {
   };
 };
 
+//Thunks
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -29,6 +32,15 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+//Reducer
 
 const initialState = { user: null };
 
