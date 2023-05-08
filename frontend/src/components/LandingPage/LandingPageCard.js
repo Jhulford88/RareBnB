12fda87c12e762e72912c62c2ad9a6f86f3fc2db
Joshua <joshua.hulford@gmail.com>
2023-05-08 15:23:30 -0400

    minor edits to landing page

--- frontend/src/components/LandingPage/LandingPageCard.js
+++ frontend/src/components/LandingPage/LandingPageCard.js
@@ -1,24 +1,27 @@
 import "./landingPage.css"
+import { Link } from "react-router-dom";
 
-function LandingPageCard(spot) {
+function LandingPageCard({spot}) {
     // console.log(spot)
     // console.log(spot.address)
     // console.log(spot.spot.previewImage)
     return (
         <li>
+            <Link to="">
             <div className="spot-card-container">
                 {/* spot picture */}
                 <div className="spot-card-image-container">
-                    <img className="spot-card-img" src={spot.spot.previewImage}></img>
+                    <img className="spot-card-img" src={spot.previewImage}></img>
                 </div>
                 {/* spot location */}
-                    <p>{spot.spot.city},{spot.spot.state}</p>
+                    <p>{spot.city},{spot.state}</p>
                 {/* spot price */}
-                    <p>${spot.spot.price} /night</p>
+                    <p>${spot.price} /night</p>
                 {/* spot rating */}
-                    <p><i class="fa-solid fa-star"></i>{(spot.spot.avgRating ? spot.spot.avgRating : "New")}</p>
+                    <p><i class="fa-solid fa-star"></i>{(spot.avgRating ? spot.avgRating : "New")}</p>
                 {/* spot name */}
             </div>
+            </Link>
         </li>
     );
 };
