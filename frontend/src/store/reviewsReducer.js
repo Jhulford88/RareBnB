9d4c525b0cae1a0af54072f20dc6e11db1baf2d1
Joshua <joshua.hulford@gmail.com>
2023-05-12 08:42:43 -0400

    Add and connect ReviewsReducer

---
+++ frontend/src/store/reviewsReducer.js
@@ -0,0 +1,40 @@
+//imports
+import { csrfFetch } from "./csrf";
+
+
+//action type constants
+
+
+
+//action creators
+
+
+
+
+
+
+
+//thunks
+
+
+
+//initial state
+const initState = {spot: {User: {}}, user: {User: {}, Spot: {}}}
+
+
+
+
+// spotsReducer
+const reviewsReducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+
+
+
+
+
+export default reviewsReducer;
