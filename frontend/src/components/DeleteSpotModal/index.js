3745b054aea0633f610fe862579e816fbc68af87
Joshua <joshua.hulford@gmail.com>
2023-07-13 15:38:58 -0400

    style delete spot modal

--- frontend/src/components/DeleteSpotModal/index.js
+++ frontend/src/components/DeleteSpotModal/index.js
@@ -1,38 +1,46 @@
 import React from "react";
 import { useModal } from "../../context/Modal";
 import { useDispatch } from "react-redux";
-import "./DeleteSpotModal.css"
+import "./DeleteSpotModal.css";
 import { deleteSpotThunk } from "../../store/spotsReducer";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 //import thunk to dispatch
 
+function DeleteSpotModal({ spot }) {
+  const { closeModal } = useModal();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-function DeleteSpotModal({spot}) {
+  const closeDeleteModal = () => {
+    return closeModal();
+  };
 
-    // console.log("spot...............",spot)
+  const deleteSpotAction = async () => {
+    await dispatch(deleteSpotThunk(spot.id));
+    history.push("/spots/current");
+    return closeModal();
+  };
 
-    const { closeModal } = useModal();
-    const dispatch = useDispatch();
-    const history = useHistory();
+  return (
+    <div className="delete-booking-modal-parent">
+      <h1>Confirm Delete</h1>
+      <h2>Are you sure you want to remove this spot from the listings?</h2>
+      <div className="delete-booking-modal-buttons-container">
+        <button
+          className="delete-booking-modal-buttons"
+          onClick={deleteSpotAction}
+        >
+          {"Yes (Delete Spot)"}
+        </button>
+        <button
+          className="delete-booking-modal-buttons"
+          onClick={closeDeleteModal}
+        >
+          {"No (Keep Spot)"}
+        </button>
+      </div>
+    </div>
+  );
+}
 
-    const closeDeleteModal = () => {
-        return closeModal()
-    }
-
-    const deleteSpotAction = async () => {
-        await dispatch(deleteSpotThunk(spot.id))
-        history.push('/spots/current')
-        return closeModal()
-    }
-
-    return (
-      <>
-        <h1>Confirm Delete</h1>
-        <h2>Are you sure you want to remove this spot from the listings?</h2>
-        <button onClick={deleteSpotAction}>{"Yes (Delete Spot)"}</button>
-        <button onClick={closeDeleteModal}>{"No (Keep Spot)"}</button>
-      </>
-    );
-  }
-
-  export default DeleteSpotModal;
+export default DeleteSpotModal;
